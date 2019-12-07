a7c20d2b57fd2d5b6eaaa4de80ac6aeb7dd7266a
Shubham <shubham@tw.local>
2019-12-07 13:16:08 +0530

    Puzzle 1.1 | sum of the fuel requirements

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import Puzzle1.RocketFuelCalculation;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Total Fuel required for all of the modules on spacecraft: " + new RocketFuelCalculation().getTotalFuel());
+    }
+}
